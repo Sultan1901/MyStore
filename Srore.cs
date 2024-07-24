a5056d26607126a36b31ff84034cda7138597475
Sultan <s.alharbi@thegarage.sa>
2024-07-24 12:37:12 +0300

    edit soo

--- Srore.cs
+++ Srore.cs
@@ -1,23 +1,23 @@
-using Microsoft.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore;
 
-namespace MyStore.Models
-{
-    public class Store
-    {
-        public int Id { get; set; }
+// namespace MyStore.Models
+// {
+//     public class Store
+//     {
+//         public int Id { get; set; }
 
-        public string Name { get; set; }
+//         public string Name { get; set; }
 
-        public string Email { get; set; }
+//         public string Email { get; set; }
 
-        public string Password { get; set; }
-    }
+//         public string Password { get; set; }
+//     }
 
-    class StoreDb : DbContext
-    {
-        public StoreDb(DbContextOptions options)
-            : base(options) { }
+//     class StoreDb : DbContext
+//     {
+//         public StoreDb(DbContextOptions options)
+//             : base(options) { }
 
-        public DbSet<Store> Stores { get; set; } = null!;
-    }
-}
+//         public DbSet<Store> Stores { get; set; } = null!;
+//     }
+// }
